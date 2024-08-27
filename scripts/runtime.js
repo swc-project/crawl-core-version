cea5e47ed57310f7dc68ae83354c80d5c4baff6b
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-28 06:50:54 +0900

    done

--- scripts/runtime.js
+++ scripts/runtime.js
@@ -9,7 +9,9 @@ import { $ } from "zx";
 import 'dotenv/config'
 import fs from 'fs/promises';
 import { cloneRepo } from './utils.js'
+import { getCoreVersions } from './commit-to-core-version.js';
 import { z } from 'zod';
+
 
 
 const RuntimeSchema = z.object({
@@ -28,6 +30,8 @@ const workspaceDir = path.resolve('.workspace');
 await fs.mkdir(workspaceDir, { recursive: true });
 
 for (const runtimeFile of await fs.readdir('pkgs/runtimes')) {
+  const packageVersions = {};
+
   const pkgYmlPath = `pkgs/runtimes/${runtimeFile}`
   const pkgYml = parseYaml(await fs.readFile(pkgYmlPath, 'utf8'));
   const runtime = RuntimeSchema.parse(pkgYml);
@@ -42,7 +46,7 @@ for (const runtimeFile of await fs.readdir('pkgs/runtimes')) {
     runtimeDir = path.join(wsDir, runtime.path)
   }
 
-  const cacheDir = path.join('cache', 'runtimes', name)
+  const cacheDir = path.join('cache', 'runtimes', runtimeFile)
   await fs.mkdir(cacheDir, { recursive: true });
 
   const cacheFile = path.join(cacheDir, 'commits.json')
@@ -59,55 +63,45 @@ for (const runtimeFile of await fs.readdir('pkgs/runtimes')) {
 
   const { defaultBranch, latestCommit } = await cloneRepo(runtime.repo, wsDir)
 
-  const repositoryRoot = (await $$`git rev-parse --show-toplevel`.text()).trim();
-  const cargoLockPath = path.resolve(`${runtimeDir}/Cargo.lock`);
-  const relativePathToCargoLock = path.relative(repositoryRoot, cargoLockPath);
+  // const repositoryRoot = (await $$`git rev-parse --show-toplevel`.text()).trim();
+  // const cargoLockPath = path.resolve(`${runtimeDir}/Cargo.lock`);
+  // const relativePathToCargoLock = path.relative(repositoryRoot, cargoLockPath);
 
-  console.log("Runtime name:", runtime.name);
-  console.log("Runtime dir:", runtimeDir);
-  console.log("Repository root:", repositoryRoot);
-  console.log("Cargo.lock path:", cargoLockPath);
-  console.log("Relative path to Cargo.lock:", relativePathToCargoLock);
+  // console.log("Runtime name:", runtime.name);
+  // console.log("Runtime dir:", runtimeDir);
+  // console.log("Repository root:", repositoryRoot);
+  // console.log("Cargo.lock path:", cargoLockPath);
+  // console.log("Relative path to Cargo.lock:", relativePathToCargoLock);
 
   // Get all git tags
-  const gitTags = (await $$`git tag`.text()).trim().split("\n").reverse();
+  const gitTags = (await $$`git tag`.text()).trim().split("\n").filter(Boolean).filter(tag => tag.startsWith("v")).reverse();
 
-  const data = {
-    runtime: runtime.name,
-    versions: [],
-  };
-
-  // For each tag, get the content of `${runtimeDir}/Cargo.lock`.
-  for (const tag of gitTags) {
-    let tagVersion = tag.replace("v", "").replace("@farmfe/core@", "");
-    if (!semver.valid(tagVersion)) {
-      console.log(`Skipping tag ${tag} because it is not a valid semver`);
-      continue;
-    }
-
-    try {
-      const cargoLock =
-        await $$`git show ${tag}:${relativePathToCargoLock}`.text();
-
-      const parsed = toml.parse(cargoLock);
-      const packages = parsed.package;
-
-      for (const pkg of packages) {
-        if (pkg.name === "swc_core") {
-          const swcCoreVersion = pkg.version;
-
-          data.versions.push({
-            version: tagVersion,
-            swcCoreVersion,
-          });
-          console.log(`Found swc_core version ${swcCoreVersion} for tag ${tag}`);
-        }
-      }
+  const coreVersions = await getCoreVersions(runtimeDir, cacheDir, gitTags);
+  console.log(coreVersions);
 
 
-    } catch (e) {
-      console.error(`Failed to parse Cargo.lock for tag ${tag}: ${e}`);
-    }
+
+  if (process.env.CRAWL_SECRET) {
+    const pkgs = []
+    const pkg = runtime.name;
+    const pkgVersions = gitTags.map((tag) => ({
+      version: tag,
+      swcCoreVersion: coreVersions[tag]
+    }));
+
+    pkgs.push({
+      name: pkg,
+      versions: pkgVersions
+    });
+
+
+    await fetch(`https://plugins.swc.rs/api/update/runtimes`, {
+      method: 'POST',
+      body: JSON.stringify({
+        token: process.env.CRAWL_SECRET,
+        pkgs,
+      })
+    })
   }
 
 
