9f54e2c55a594a5f5100ddf2f2c7c3138a78fc6a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-27 16:49:11 +0900

    init

---
+++ scripts/wasm-plugin.mjs
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+// We iterate over all commits from the main branch, in reverse order
+//
+// We grab the version from the package.json files, and use it to determine the commit for a given npm version
