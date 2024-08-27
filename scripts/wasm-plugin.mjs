d5f603e4b570d5572a2247b053c38c4486093b85
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-27 17:12:42 +0900

    clone

--- scripts/wasm-plugin.mjs
+++ scripts/wasm-plugin.mjs
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 import { $ } from "zx";
 import fs from 'node:fs/promises'
-import { parse as parseYaml,  } from 'yaml'
-import {z }from 'zod'
+import { parse as parseYaml, } from 'yaml'
+import { z } from 'zod'
 import path from "node:path";
 
 // We iterate over all commits from the main branch, in reverse order
 //
 // We grab the version from the package.json files, and use it to determine the commit for a given npm version
 
-const workspaceDir= path.resolve('.workspace');
+const workspaceDir = path.resolve('.workspace');
 await fs.mkdir(workspaceDir, { recursive: true });
 
 const PluginSchema = z.object({
@@ -17,10 +17,19 @@ const PluginSchema = z.object({
     packages: z.array(z.string())
 })
 
-for(const pkg of await fs.readdir('pkgs/plugins')) {
+for (const pkg of await fs.readdir('pkgs/plugins')) {
+    const { name } = path.parse(pkg);
+
     const pkgYmlPath = `pkgs/plugins/${pkg}`
     const pkgYml = parseYaml(await fs.readFile(pkgYmlPath, 'utf8'));
     const plugin = PluginSchema.parse(pkgYml);
 
-    console.log(plugin)
+    const wsDir = path.join(workspaceDir, name)
+    await fs.mkdir(wsDir, { recursive: true });
+
+    console.info(`Cloning ${plugin.repo} into ${wsDir}`)
+
+    const $$ = $({ cwd: wsDir });
+    
+    await $$`git clone ${plugin.repo} .`
 }
